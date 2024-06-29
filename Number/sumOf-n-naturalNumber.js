2e523a508d9c2bb1d5130c016916081b3522ada6
dev-aswinreji <aswinreji.004@gmail.com>
2024-06-29 19:53:54 +0530

    feat: Summation with Time complexity constant 1 using Arithametic Progression formula

--- Number/sumOf-n-naturalNumber.js
+++ Number/sumOf-n-naturalNumber.js
@@ -9,3 +9,12 @@ function sumOfNnaturalNumber(n) {
 
 const sum = sumOfNnaturalNumber(7)
 console.log(sum);
+
+//Reduce time complexity of this code by using Arithametic Progession Formula by Fredric Gauss
+
+function summationWithTimeComplexityConstant(n){
+    return (n*(n+1))/2
+}
+
+let summation = summationWithTimeComplexityConstant(40)
+console.log(summation,'summation');

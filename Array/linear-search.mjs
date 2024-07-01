6c940fbd0c99d126f04083e045304c806ad09630
dev-aswinreji <aswinreji.004@gmail.com>
2024-07-01 21:19:07 +0530

    feat: Linear search Big-O O(n)

---
+++ Array/linear-search.mjs
@@ -0,0 +1,15 @@
+
+const array = [2, 3, 5, 6, 7, 8]
+const target = 6
+
+function findElementExistInArray(array, target) {
+    
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === target)
+            return i
+    }
+    return -1
+}
+
+console.log(findElementExistInArray(array, target));
+

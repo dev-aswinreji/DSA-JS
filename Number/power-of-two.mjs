c855e1df1808568eeaaa9246c8de6a3ca9d0cdd9
dev-aswinreji <aswinreji.004@gmail.com>
2024-07-01 19:09:08 +0530

    feat: Power of Two Using bitWise Operator

--- Number/power-of-two.mjs
+++ Number/power-of-two.mjs
@@ -1,10 +1,13 @@
 
+
+// Using for loop
+
 // function isPowerOf2 (n) {
 //     if(n < 1){
 //         return false
 //     }
 //     let num = n
-//     for(let i = 0; i<n ;i++){  //using for loop
+//     for(let i = 0; i<n ;i++){  
 //         if(num%2==0){
 //             num = num/2              
 //         }
@@ -15,29 +18,49 @@
 // }
 
 
+// Using while loop
 
+// function isPowerOf2(n) {
+//     if (n < 1) {
+//         return false
+//     }
 
-function isPowerOf2(n) {
-    if (n < 1) {
-        return false
-    }
+//     while (n > 1) {
+//         if (n % 2 !== 0) {
+//             return false
+//         }
+//         n = n / 2
+//     }
+//     return true
+// }
 
-    while (n > 1) {
-        if (n % 2 !== 0) {
-            return false
-        }
-        n = n / 2
-    }
-    return true
-}
-
-//Big O = O(logn) In each iteration we are reducing the value of n by half 
+// Big O = O(logn) In each iteration we are reducing the value of n by half 
 // Input size is reduced by half - O(logn)
 
-console.log(isPowerOf2(2)); //true
+// console.log(isPowerOf2(2)); //true
 
-console.log(isPowerOf2(4)); //true
+// console.log(isPowerOf2(4)); //true
 
-console.log(isPowerOf2(7)); //false
+// console.log(isPowerOf2(7)); //false
 
-console.log(isPowerOf2(8)); //true
+// console.log(isPowerOf2(8)); //true
+
+
+// There does exist a solution with constant time complexity
+// Which is a one-liner
+// Solution involves use of bit-wise operator 
+
+function isPowerOf2BitWise (n) {
+    if(n < 1){
+        return false
+    }
+    return (n & (n-1)) === 0
+}
+
+console.log(isPowerOf2BitWise(2)); //true
+
+console.log(isPowerOf2BitWise(4)); //true
+
+console.log(isPowerOf2BitWise(7)); //false
+
+console.log(isPowerOf2BitWise(8)); //true

5fce59543fbc40628994ebd77793367960f4f329
dev-aswinreji <aswinreji.004@gmail.com>
2024-07-01 18:52:14 +0530

    feat: Power of 2 using for loop and while loop

---
+++ Number/power-of-two.mjs
@@ -0,0 +1,41 @@
+
+// function isPowerOf2 (n) {
+//     if(n < 1){
+//         return false
+//     }
+//     let num = n
+//     for(let i = 0; i<n ;i++){  //using for loop
+//         if(num%2==0){
+//             num = num/2              
+//         }
+//     }
+//     if(num === 1 )
+//         return true
+//     return false
+// }
+
+
+
+
+function isPowerOf2(n) {
+    if (n < 1) {
+        return false
+    }
+
+    while (n > 1) {
+        if (n % 2 !== 0) {
+            return false
+        }
+        n = n / 2
+    }
+    return true
+}
+
+
+console.log(isPowerOf2(2)); //true
+
+console.log(isPowerOf2(4)); //true
+
+console.log(isPowerOf2(7)); //false
+
+console.log(isPowerOf2(8)); //true

d86ae001b2307fdbc37945f7e514aaac92923d36
dev-aswinreji <aswinreji.004@gmail.com>
2024-07-11 21:02:39 +0530

    feat: DSA Problems solving Doubly linked list

--- Array/linear-search.mjs
+++ Array/linear-search.mjs
@@ -1,16 +1,3 @@
-
-const array = [2, 3, 5, 6, 7, 8]
-const target = 6
-
-function findElementExistInArray(array, target) {
-    
-    for (let i = 0; i < array.length; i++) {
-        if (array[i] === target)
-            return i
-    }
-    return -1
-}
-
 console.log(findElementExistInArray(array, target));
 
 // Ouput : 3
---
+++ Problems-DSA/doubly-linked-list.mjs
@@ -0,0 +1,89 @@
+
+
+class Node{
+  constructor(value){
+    this.value = value
+    this.next = null
+    this.prev = null
+  }
+}
+
+class LinkedList{
+  constructor() {
+    this.head = null
+    this.tail = null
+    this.length = 0
+  }
+
+  isEmpty(){
+    return this.length === 0
+  }
+
+  getLength(){
+    return this.length
+  }
+
+  prepend(value){
+    const newNode = new Node(value)
+
+    if(this.isEmpty()){
+      this.head = newNode
+      this.tail = newNode
+    }else{
+      newNode.next = this.head
+      this.head.prev = newNode
+      this.head = newNode
+    }
+    this.length++
+  }
+
+  insert(value,index){
+    if(index < 0 || index > this.length){
+      console.log('Invalid index');
+    }
+    if(index === 0){
+      this.prepend(value)
+    }else{
+      const newNode = new Node(value)
+      let prevNode = this.head
+      for(let i = 0; i<index -1 ; i++){
+        prevNode = prevNode.next
+      }
+      let nextNode = prevNode.next
+
+      newNode.next = nextNode
+      newNode.prev = prevNode
+      nextNode.prev = newNode
+      prevNode.next = newNode
+      this.length++
+    }
+  }
+
+  print(){
+    if(this.isEmpty()){
+      console.log('List is empty');
+      return 
+    }else{
+      let curr = this.head
+      console.log(curr,'what ');
+      let list = ''
+
+      while(curr){
+        list += curr.value + ' '
+        curr = curr.next
+      }
+      console.log(list,'list is showing');
+    }
+  }
+}
+
+
+const list = new LinkedList()
+
+list.prepend(10)
+
+list.prepend(20)
+
+list.insert(15,1)
+
+list.print()

5150424dbdb1248b658629417c3c690ac1f39410
dev-aswinreji <aswinreji.004@gmail.com>
2024-07-12 14:33:12 +0530

    feat: DSA Circular Linked List need to be learn

---
+++ Circular-Linked-List/circular-linked-list.mjs
@@ -0,0 +1,20 @@
+
+
+class Node {
+  constructor (value) {
+    this.prev = null
+    this.value = value
+    this.next = null
+  }
+}
+
+
+class CicularLinkedList {
+  constructor () {
+   this.head = null
+    this.tail = null
+    this.size = 0
+  }
+
+
+};

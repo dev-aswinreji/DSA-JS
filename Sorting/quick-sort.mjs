d0edf825497e04a1fbe1c1aa68c1beab10b66058
dev-aswinreji <aswinreji.004@gmail.com>
2024-07-15 21:23:07 +0530

    feat: Quick Sort Started

---
+++ Sorting/quick-sort.mjs
@@ -0,0 +1,15 @@
+
+
+const array = [3,5,6,2,7,1,4]
+const n = array.length
+function quickSort(arr,n) {
+ 
+  let min_ind,i,j
+  for(i = 0; i<n-1;i++){
+
+    for(let j= i +1; j < n; j++){
+      
+    }
+
+  }
+}

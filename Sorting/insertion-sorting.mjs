d0c056698ad8d9ea4ae8e706a56a79641c3ffef5
dev-aswinreji <aswinreji.004@gmail.com>
2024-07-15 17:55:39 +0530

    feat: Insertion Sort

--- Sorting/insertion-sorting.mjs
+++ Sorting/insertion-sorting.mjs
@@ -1,1 +1,22 @@
 
+const array = [23, 1, 10, 5, 2]
+
+function insertionSort(arr, n) {
+  let i, key, j
+  for (i = 1; i < n; i++) {
+    key = arr[i]
+    j = i - 1
+
+    while(j >= 0 && arr[j] > key){
+      arr[j+1] = arr[j]
+      j--
+    }
+    arr[j+1] = key
+  }
+}
+
+const result = insertionSort(array, array.length)
+
+console.log(array, 'result is showing')
+
+

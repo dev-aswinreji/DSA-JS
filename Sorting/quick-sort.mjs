060f6d52fa0a7da24523e1caeb44b28fae4a3243
dev-aswinreji <aswinreji.004@gmail.com>
2024-07-16 22:25:35 +0530

    feat: Quick Sort

--- Sorting/quick-sort.mjs
+++ Sorting/quick-sort.mjs
@@ -1,15 +1,35 @@
 
+const array = [8,3,2,7,6,1,4]
 
-const array = [3,5,6,2,7,1,4]
-const n = array.length
-function quickSort(arr,n) {
- 
-  let min_ind,i,j
-  for(i = 0; i<n-1;i++){
-
-    for(let j= i +1; j < n; j++){
-      
-    }
-
+const N = array.length
+let low = 0 
+let high = N - 1
+function quickSort(arr,low,high) {
+  
+  if(low < high){
+    let pi = partition(arr,low,high)
+    quickSort(arr,pi + 1, high)
+    quickSort(arr,low,pi - 1)
   }
 }
+
+
+function partition(arr,low,high) {
+ 
+  let pivot = arr[high]
+  let start = low - 1 
+
+  for(let j = low ; j<= high; j++){
+    if(arr[j]< pivot){
+      start++
+      [arr[start],arr[j]] = [arr[j],arr[start]]
+    }
+  }
+  [arr[start+1],arr[high]] = [arr[high], arr[start+1]]
+  return start + 1
+}
+
+quickSort(array, low,high)
+
+console.log(array,'Sorted array is showing')
+

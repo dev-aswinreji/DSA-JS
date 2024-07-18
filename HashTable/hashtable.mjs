866094f6d04f1a0a6cd8a15bbad8d50ccb6dd332
dev-aswinreji <aswinreji.004@gmail.com>
2024-07-18 20:01:57 +0530

    feat: Hash Table Implementation

--- HashTable/hashtable.mjs
+++ HashTable/hashtable.mjs
@@ -1,34 +1,68 @@
 
 class HashTable {
-  constructor (size) {
+  constructor(size) {
     this.table = new Array(size)
-    this.size = size    
+    this.size = size
   }
 
-  hash(key){
-    let total = 0 
-    for(let i = 0 ; i<key.length; i++){
-      console.log(key.charCodeAt(i),'charAt')
+  hash(key) {
+    let total = 0
+    for (let i = 0; i < key.length; i++) {
       total += key.charCodeAt(i)
     }
-    console.log(total,'total is showing')
-    let data = total % this.size
-    console.log(data,'data is showing')
-    return data
+    return total % this.size
   }
 
-  set(key,value){
+  set(key, value) {
+    console.log(key,'vlu/',value);
     const index = this.hash(key)
-    this.table[index] = value
+    //     this.table[index] = value
+    console.log(index,'ind');
+    let bucket = this.table[index]
+    if (!bucket) {
+      this.table[index] = [[key, value]]
+    } else {
+      const sameKeyItem = bucket.find((item) => item[0] === key)
+      if (sameKeyItem) {
+        sameKeyItem[1] = value
+      } else {
+        bucket.push([key, value]);
+      }
+    }
+    console.log(bucket,'bucket is showing');
   }
 
-  get(key){
+  get(key) {
     const index = this.hash(key)
-    return this.table[index]
+    console.log(index,'ind');
+    console.log(this.table,'table is showing');
+
+    //     return this.table[index]
+    const bucket = this.table[index]
+    if (bucket) {
+      const sameKeyItem = bucket.find(item => item[0] === key)
+      if (sameKeyItem) {
+        return sameKeyItem[1]
+      }
+    }
+    return undefined
   }
+
+  remove(key) {
+    const index = this.hash(key)
+    const bucket = this.table[index]
+    if (bucket) {
+      const sameKeyItem = bucket.find(item => item[0] === key)
+      if (sameKeyItem) {
+        bucket.splice(bucket.indexOf(sameKeyItem), 1)
+
+      }
+    }
+  }
+
 }
 
 const table = new HashTable(40)
 
-table.set("name","bruce")
+table.set("name", "bruce")
 console.log(table.get("name"))

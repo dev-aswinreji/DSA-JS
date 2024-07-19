9e8d3242e516df27fb87c6c1407ca0c58b85ace9
dev-aswinreji <aswinreji.004@gmail.com>
2024-07-19 13:28:44 +0530

    feat: bubble sort

---
+++ Problems-DSA/sorting.mjs
@@ -0,0 +1,15 @@
+// bubble sort
+
+const array = [7,8,5,2,9]
+
+const n = array.length
+
+function bubbleSort(arr,n){
+
+    let swapped = true
+
+    while(swapped){
+        swapped = true
+        for()
+    }
+}

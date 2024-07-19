80b12c8bf36f1744c6c092d14eb274dd1a615a94
dev-aswinreji <aswinreji.004@gmail.com>
2024-07-19 10:10:34 +0530

    feat: Stack Using Linked List implementation

---
+++ Stack-Using-LL/stack-using-LL.mjs
@@ -0,0 +1,75 @@
+
+class Node {
+    constructor(value) {
+        this.value = value
+        this.next = null
+    }
+}
+
+
+class Stack {
+    constructor() {
+        this.top = null
+        this.size = 0
+    }
+
+    isEmpty() {
+        return this.size === 0
+    }
+
+    push(value) {
+        const newNode = new Node(value)
+        if (this.isEmpty()) {
+            this.top = newNode
+        } else {
+            newNode.next = this.top
+            this.top = newNode
+        }
+        this.size++
+    }
+
+    pop() {
+        let removeNode = this.top
+        this.top = removeNode.next
+        removeNode = null
+        this.size--
+    }
+
+    reverse() {
+        let curr = this.top
+        let prev = null
+        while (curr) {
+            let next = curr.next
+            curr.next = prev
+            prev = curr
+            console.log(prev,'prev');
+            curr = next
+        }
+        this.top = prev
+    }
+    
+    print() {
+        let curr = this.top
+        let str = ''
+
+        while (curr) {
+            str += `${curr.value} `
+            curr = curr.next
+        }
+
+        console.log('List :', str);
+    }
+}
+
+const stack = new Stack()
+
+
+stack.push(40)
+
+stack.push(50)
+
+// stack.pop()
+stack.print()
+stack.reverse()
+
+stack.print()

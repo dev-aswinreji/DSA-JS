2f0cd22e9bb43e0e047e5c0c69f1325ba3c08120
dev-aswinreji <aswinreji.004@gmail.com>
2024-07-25 20:06:04 +0530

    feat: Binary Search Tree

---
+++ Binary-Search-Tree/binary-search-tree.mjs
@@ -0,0 +1,80 @@
+
+class Node {
+    constructor(value) {
+        this.value = value
+        this.left = null
+        this.right = null
+    }
+}
+
+class BinarySearchTree {
+    constructor() {
+        this.root = null
+    }
+
+    isEmpty() {
+        return this.root === null
+    }
+
+    insert(value) {
+        const newNode = new Node(value)
+        if (this.isEmpty()) {
+            this.root = newNode
+        } else {
+            this.insertNode(this.root, newNode)
+        }
+    }
+
+    insertNode(root, newNode) {
+        if (newNode.value < root.value) {
+            if (root.left === null) {
+                root.left = newNode
+            } else {
+                this.insertNode(root.left, newNode)
+            }
+        } else {
+            if (root.right === null) {
+                root.right = newNode
+            } else {
+                this.insertNode(root.right, newNode)
+            }
+        }
+
+    }
+
+    search(root, value) {
+        if (!root) {
+            console.log('root is empty or invalid root');
+            return
+        } else {
+            if (root.value === value) {
+                console.log(value, 'found');
+                return
+            } else if (value < root.value) {
+                this.search(root.left, value)
+            } else {
+                this.search(this.right, value)
+            }
+        }
+    }
+
+    print() {
+        console.log(this.root)
+    }
+}
+
+
+const bst = new BinarySearchTree()
+
+bst.print()
+
+bst.insert(10)
+bst.insert(5)
+
+bst.insert(15)
+bst.insert(7)
+bst.insert(4)
+
+bst.search(bst, 6)
+bst.print()
+

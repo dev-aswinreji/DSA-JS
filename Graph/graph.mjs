954e4bcf158d9254db1d5f1322bbf3502e567681
Aswin Reji <aswinreji.004@gmail.com>
2024-07-29 20:52:27 +0530

    feat: Graph Ajacent matrix representation

--- Graph/graph.mjs
+++ Graph/graph.mjs
@@ -1,4 +1,10 @@
 
+//      (A)
+//    /    \
+//   /      \
+// (B) ---- (C)
+
+
 // Adjacency Matrix of Undirected Graph
 const matrix = [
   // A,B,C
@@ -8,4 +14,22 @@ const matrix = [
 ]
 
 console.log(matrix[0][1]);  // A-B 
+
+
+
+//     (A)
+//    /   \
+//   /     \
+// (B)     (C)
+
+
+// This is our Graph Structure and we are going to make a Adjacency Metrix
+
+const adjacencyMatrix = [
+  // A,B,C 
+    [0,1,0],//A
+    [1,0,1],//B
+    [0,1,0] //C
+]
+
 

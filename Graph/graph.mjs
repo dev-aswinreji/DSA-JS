b108deed0d90aa9d7d6e8eec62aba23421b487af
Aswin Reji <aswinreji.004@gmail.com>
2024-07-29 22:41:37 +0530

    feat: Graph Adjacency Matrix Adjacency List Making Adjacency List Graph

--- Graph/graph.mjs
+++ Graph/graph.mjs
@@ -1,4 +1,98 @@
 
+class Graph {
+  constructor () {
+  this.adjacencyList = {} 
+  }
+
+  addVertex(vertex){
+    if(!this.adjacencyList[vertex]){
+      this.adjacencyList[vertex]  = new Set()
+    }
+  }
+
+  addEdges(vertex1,vertex2){
+    if(!this.adjacencyList[vertex1]){
+      this.addVertex(vertex1)
+    }
+    if(!this.adjacencyList[vertex2]){
+      this.addVertex(vertex2)
+    }
+
+     this.adjacencyList[vertex1].add(vertex2)
+     this.adjacencyList[vertex2].add(vertex1)
+  }
+
+  hasEdge(vertex1,vertex2){
+    return (
+      this.adjacencyList[vertex1].has(vertex2) &&
+      this.adjacencyList[vertex2].has(vertex1)
+    )
+  }
+
+  removeEdge(vertex1,vertex2){
+      this.adjacencyList[vertex1].delete(vertex2)
+      this.adjacencyList[vertex2].delete(vertex1)
+  }
+
+  removeVertex(vertex){
+    if(!this.adjacencyList[vertex]){
+      return
+    }else{
+    for(let adjacentVertex of this.adjacencyList[vertex])
+      this.removeEdge(vertex,adjacentVertex)
+    }
+
+    delete this.adjacencyList[vertex]
+  }
+
+  display(){
+    for (const vertex in this.adjacencyList) {
+       console.log(vertex + '->' + [...this.adjacencyList[vertex]]) 
+    }
+  }
+}
+
+
+const graph = new Graph()
+
+graph.addVertex('A')
+graph.addVertex('B')
+graph.addVertex('C')
+
+graph.addEdges("A","B")
+
+graph.addEdges("B","C")
+
+graph.display()
+
+console.log(graph.hasEdge("A","B"))
+
+graph.removeEdge("A","B")
+
+console.log(graph.hasEdge("A","B"))
+graph.removeVertex("B")
+graph.display()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //      (A)
 //    /    \
 //   /      \
@@ -32,4 +126,13 @@ const adjacencyMatrix = [
     [0,1,0] //C
 ]
 
+// Adjacency List 
+
+const adjacencyList = {
+  'A':['B'],
+  'B':['A','C'],
+  'C':['B']
+}
+
+console.log(adjacencyList['C'])
 
